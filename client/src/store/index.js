34966e3e997373d9930faac83524ef217d678b37
Jodu555 <Jodu505@gmail.com>
2022-01-05 12:18:10 +0100

    Setted up the vuex store with the name computed

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: () => ({
-    name: 'Anonymous',
+    name: null,
     messages: []
   }),
   mutations: {
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,28 @@
 <template>
-	<div class="home"></div>
+	<div>
+		<input v-model="name" type="text" placeholder="Your Name" />
+		<div v-if="name">
+			<h1>Hello World</h1>
+		</div>
+	</div>
 </template>
 
 <script>
+import { mapMutations, mapActions, mapState } from 'vuex';
+
 export default {
 	name: 'Home',
+	created() {},
+	computed: {
+		...mapState(['messages']),
+		name: {
+			get() {
+				return this.$store.state.name;
+			},
+			set(value) {
+				this.$store.commit('setName', value);
+			},
+		},
+	},
 };
 </script>
