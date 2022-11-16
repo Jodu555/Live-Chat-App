7cfd58662027a82fb891f13d68374b23a02f3b3f
Jodu555 <Jodu505@gmail.com>
2022-11-16 16:37:26 +0100

    Started to opt in typescript to get a bit hands on

--- Server/build/index.js
+++ Server/build/index.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const http = require('http');
 const express = require('express');
 const { Server } = require("socket.io");
@@ -6,35 +15,28 @@ const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const dotenv = require('dotenv').config();
-
 const { Database } = require('@jodu555/mysqlapi');
-
 const database = Database.createDatabase('localhost', 'root', '', 'rt-chat');
 database.connect();
 require('./utils/database')();
-
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(express.json());
-
-
-
 let server;
 if (process.env.https) {
     const sslProperties = {
         key: fs.readFileSync(process.env.KEY_FILE),
         cert: fs.readFileSync(process.env.CERT_FILE),
     };
-    server = https.createServer(sslProperties, app)
-} else {
+    server = https.createServer(sslProperties, app);
+}
+else {
     server = http.createServer(app);
 }
-
 let actionbar = '';
 const typer = {};
-
 const io = new Server(server, {
     cors: {
         origin: "*",
@@ -43,16 +45,17 @@ const io = new Server(server, {
 });
 io.on("connection", (socket) => {
     console.log('Connection');
-    socket.on('newMessage', async (obj) => {
+    socket.on('newMessage', (obj) => __awaiter(this, void 0, void 0, function* () {
         try {
             const validate = database.getSchema('message').validate(obj, true);
             const message = validate.object;
-            await database.get('messages').create(message);
+            yield database.get('messages').create(message);
             io.emit('newMessage', message);
-        } catch (error) {
+        }
+        catch (error) {
             console.log('Error: ', error);
         }
-    });
+    }));
     socket.on('action', (data) => {
         if (data.type == 'typing') {
             if (data.info == 'started') {
@@ -64,9 +67,8 @@ io.on("connection", (socket) => {
                 calcAndSendActionbar();
             }
         }
-    })
+    });
 });
-
 function calcAndSendActionbar() {
     let actionbar = '';
     if (Object.keys(typer).length == 0) {
@@ -76,47 +78,40 @@ function calcAndSendActionbar() {
     Object.keys(typer).forEach(name => {
         actionbar += name + ', ';
     });
-
     actionbar += ' is currently typing';
     io.emit('actionbar', actionbar);
 }
-
-
 app.get('/', (req, res) => {
-    res.json({ message: 'Working API' })
+    res.json({ message: 'Working API' });
 });
-
-app.get('/messages', async (req, res, next) => {
+app.get('/messages', (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     try {
-        let response = await database.get('messages').get();
+        let response = yield database.get('messages').get();
         response = response.sort((a, b) => b.created_at - a.created_at);
         res.json(response);
-    } catch (error) {
-        next(error)
     }
-});
-
-app.post('/messages', async (req, res, next) => {
+    catch (error) {
+        next(error);
+    }
+}));
+app.post('/messages', (req, res, next) => __awaiter(this, void 0, void 0, function* () {
     try {
         const validate = database.getSchema('message').validate(req.body, true);
         const message = validate.object;
-        const response = await database.get('messages').create(message);
+        const response = yield database.get('messages').create(message);
         res.json(response);
-    } catch (error) {
+    }
+    catch (error) {
         console.log('Error: ', error);
         next(error);
     }
-});
-
+}));
 app.use((err, req, res, next) => {
     res.json(err);
-})
-
-app.use((req, res) => {
-    res.json({ message: 'NotFound' })
 });
-
-
+app.use((req, res) => {
+    res.json({ message: 'NotFound' });
+});
 const PORT = process.env.PORT || 3100;
 server.listen(PORT, () => {
     console.log(`Express App Listening ${process.env.https ? 'with SSL ' : ''}on ${PORT}`);
---
+++ Server/src/index.ts
@@ -0,0 +1,123 @@
+import fs from 'fs';
+import http from 'http';
+import express from 'express';
+import { Server } from 'socket.io';
+import https from 'https';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Database } from '@jodu555/mysqlapi';
+
+const database = Database.createDatabase('localhost', 'root', '', 'rt-chat');
+database.connect();
+const databaseModule = require('./utils/database');
+// import * as databaseModule from './utils/database';
+databaseModule();
+
+const app = express();
+app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(express.json());
+
+let server: http.Server | https.Server;
+if (process.env.https) {
+	const sslProperties = {
+		key: fs.readFileSync(process.env.KEY_FILE as string),
+		cert: fs.readFileSync(process.env.CERT_FILE as string),
+	};
+	server = https.createServer(sslProperties, app);
+} else {
+	server = http.createServer(app);
+}
+
+let actionbar = '';
+const typer = {};
+
+const io = new Server(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
+});
+io.on('connection', (socket) => {
+	console.log('Connection');
+	socket.on('newMessage', async (obj) => {
+		try {
+			const validate = database.getSchema('message').validate(obj, true);
+			const message = validate.object;
+			await database.get('messages').create(message);
+			io.emit('newMessage', message);
+		} catch (error) {
+			console.log('Error: ', error);
+		}
+	});
+	socket.on('action', (data) => {
+		if (data.type == 'typing') {
+			if (data.info == 'started') {
+				typer[data.name] = true;
+				calcAndSendActionbar();
+			}
+			if (data.info == 'stopped') {
+				delete typer[data.name];
+				calcAndSendActionbar();
+			}
+		}
+	});
+});
+
+function calcAndSendActionbar(): void {
+	let actionbar = '';
+	if (Object.keys(typer).length == 0) {
+		io.emit('actionbar', '');
+		return;
+	}
+	Object.keys(typer).forEach((name) => {
+		actionbar += name + ', ';
+	});
+
+	actionbar += ' is currently typing';
+	io.emit('actionbar', actionbar);
+}
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Working API' });
+});
+
+app.get('/messages', async (req, res, next) => {
+	try {
+		let response = await database.get('messages').get();
+		response = response.sort((a, b) => b.created_at - a.created_at);
+		res.json(response);
+	} catch (error) {
+		next(error);
+	}
+});
+
+app.post('/messages', async (req, res, next) => {
+	try {
+		const validate = database.getSchema('message').validate(req.body, true);
+		const message = validate.object;
+		const response = await database.get('messages').create(message);
+		res.json(response);
+	} catch (error) {
+		console.log('Error: ', error);
+		next(error);
+	}
+});
+
+app.use((err, req, res, next) => {
+	res.json(err);
+});
+
+app.use((req, res) => {
+	res.json({ message: 'NotFound' });
+});
+
+const PORT = process.env.PORT || 3100;
+server.listen(PORT, () => {
+	console.log(`Express App Listening ${process.env.https ? 'with SSL ' : ''}on ${PORT}`);
+});
