8943f2faf422bf56105f164d91158f1164f25700
Jodu555 <Jodu505@gmail.com>
2022-11-16 19:16:12 +0100

    added the whole ting database description WIP

--- Server/src/mod.d.ts
+++ Server/src/mod.d.ts
@@ -39,6 +39,18 @@ declare module '@jodu555/mysqlapi' {
 	interface Database {
 		connect: () => void;
 		createTable: (tablename: string, table: TableObject) => void;
+		registerSchema: (name: string, schema: object, reference_table_name: string) => void;
+		get: (thing: string) => thingDatabase;
+		getSchema: (name: string) => any;
+	}
+
+	interface thingDatabase {
+		create: (thing: object) => Promise<object>;
+		update: (search: object, thing: object) => void;
+		getOne: (search: object) => Promise<object>;
+		get: (search?: object) => Promise<[object]>;
+		delete: (search: object) => void;
+		getLatest: (action: 'inserted' | 'updated' | 'deleted', search: object) => Promise<object>;
 	}
 
 	export let Database: IOverDatabase;
